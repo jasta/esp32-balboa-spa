8c832e2edff330ab1575dc330a55a3c44ae4d8b5
jasta <jasta@devtcg.org>
2023-01-18 13:01:16 -0800

    Beginning of a proper structuring to topside_panel app

--- esp32-app/src/bin/topside_panel.rs
+++ esp32-app/src/bin/topside_panel.rs
@@ -1,3 +1,4 @@
+use std::io::{Read, Write};
 use anyhow::anyhow;
 use balboa_spa_messages::channel::Channel;
 use balboa_spa_messages::framed_reader::FramedReader;
@@ -22,63 +23,85 @@ fn main() -> anyhow::Result<()> {
       peripherals.pins.gpio27,
       Some(peripherals.pins.gpio13))?;
 
-  let (rx, tx) = transport.split();
-  let mut reader = FramedReader::new(rx)
-      .set_debug(true);
-  let mut writer = FramedWriter::new(tx);
+  let panel = TopsidePanel::new(transport);
+  panel.run_read_test()?;
+  Ok(())
+}
 
-  loop {
-    let message = reader.next_message()?;
-    info!("Got {message:?}");
+struct TopsidePanel<R, W> {
+  reader: FramedReader<R>,
+  writer: FramedWriter<W>,
+}
 
-    let mut state = GetVersionTestState::NeedChannelWaitingCTS;
-    let mut my_channel = None;
+impl<R: Read, W: Write> TopsidePanel<R, W> {
+  pub fn new(transport: impl Transport<R, W>) -> Self {
+    let (raw_reader, raw_writer) = transport.split();
+    let reader = FramedReader::new(raw_reader);
+    let writer = FramedWriter::new(raw_writer);
+    Self { reader, writer }
+  }
 
-    match MessageType::try_from(&message) {
-      Ok(mt) => {
-        match (message.channel, mt) {
-          (Channel::MulticastChannelAssignment, MessageType::NewClientClearToSend()) => {
-            assert_eq!(state, GetVersionTestState::NeedChannelWaitingCTS);
-            writer.write(
-              &MessageType::ChannelAssignmentRequest {
-                device_type: 0x0,
-                client_hash: 0xcafe,
-              }.to_message(Channel::MulticastChannelAssignment)?)?;
-            state = GetVersionTestState::NeedChannelWaitingAssignment;
-          }
-          (Channel::MulticastChannelAssignment, MessageType::ChannelAssignmentResponse { channel, .. }) => {
-            assert_eq!(state, GetVersionTestState::NeedChannelWaitingAssignment);
-            my_channel = Some(channel);
-            writer.write(&MessageType::ChannelAssignmentAck().to_message(channel)?)?;
-            state = GetVersionTestState::NeedInfoWaitingCTS;
-          }
-          (channel, MessageType::ClearToSend()) => {
-            if my_channel == Some(channel) {
-              match state {
-                GetVersionTestState::NeedInfoWaitingCTS => {
-                  writer.write(
-                    &MessageType::SettingsRequest(SettingsRequestMessage::Information)
-                        .to_message(channel)?)?;
-                  state = GetVersionTestState::NeedInfoWaitingInfo;
-                }
-                _ => {
-                  writer.write(&MessageType::NothingToSend().to_message(channel)?)?;
+  pub fn run_read_test(mut self) -> anyhow::Result<()> {
+    loop {
+      let message = self.reader.next_message()?;
+      info!("Got {message:?}");
+    }
+  }
+
+  pub fn run_loop(mut self) -> anyhow::Result<()> {
+    loop {
+      let message = self.reader.next_message()?;
+      info!("Got {message:?}");
+
+      let mut state = GetVersionTestState::NeedChannelWaitingCTS;
+      let mut my_channel = None;
+
+      match MessageType::try_from(&message) {
+        Ok(mt) => {
+          match (message.channel, mt) {
+            (Channel::MulticastChannelAssignment, MessageType::NewClientClearToSend()) => {
+              assert_eq!(state, GetVersionTestState::NeedChannelWaitingCTS);
+              self.writer.write(
+                &MessageType::ChannelAssignmentRequest {
+                  device_type: 0x0,
+                  client_hash: 0xcafe,
+                }.to_message(Channel::MulticastChannelAssignment)?)?;
+              state = GetVersionTestState::NeedChannelWaitingAssignment;
+            }
+            (Channel::MulticastChannelAssignment, MessageType::ChannelAssignmentResponse { channel, .. }) => {
+              assert_eq!(state, GetVersionTestState::NeedChannelWaitingAssignment);
+              my_channel = Some(channel);
+              self.writer.write(&MessageType::ChannelAssignmentAck().to_message(channel)?)?;
+              state = GetVersionTestState::NeedInfoWaitingCTS;
+            }
+            (channel, MessageType::ClearToSend()) => {
+              if my_channel == Some(channel) {
+                match state {
+                  GetVersionTestState::NeedInfoWaitingCTS => {
+                    self.writer.write(
+                      &MessageType::SettingsRequest(SettingsRequestMessage::Information)
+                          .to_message(channel)?)?;
+                    state = GetVersionTestState::NeedInfoWaitingInfo;
+                  }
+                  _ => {
+                    self.writer.write(&MessageType::NothingToSend().to_message(channel)?)?;
+                  }
                 }
               }
             }
+            (channel, MessageType::InformationResponse(info)) => {
+              assert_eq!(state, GetVersionTestState::NeedInfoWaitingInfo);
+              assert_eq!(Some(channel), my_channel);
+              info!("Got system model number: {}", info.system_model_number);
+            }
+            (channel, MessageType::StatusUpdate(status)) => {
+              // Ignore...
+            }
+            _ => warn!("Unhandled: {message:?}"),
           }
-          (channel, MessageType::InformationResponse(info)) => {
-            assert_eq!(state, GetVersionTestState::NeedInfoWaitingInfo);
-            assert_eq!(Some(channel), my_channel);
-            info!("Got system model number: {}", info.system_model_number);
-          }
-          (channel, MessageType::StatusUpdate(status)) => {
-            // Ignore...
-          }
-          _ => warn!("Unhandled: {message:?}"),
         }
+        Err(e) => error!("{e:?}"),
       }
-      Err(e) => error!("{e:?}"),
     }
   }
 }

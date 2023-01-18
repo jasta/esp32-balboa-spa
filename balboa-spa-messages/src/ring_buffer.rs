a31aa20378067f6b21970901018b6226715aa533
jasta <jasta@devtcg.org>
2023-01-18 12:59:19 -0800

    Improved ByteRingBuffer Debug formatting

--- balboa-spa-messages/src/ring_buffer.rs
+++ balboa-spa-messages/src/ring_buffer.rs
@@ -37,10 +37,9 @@ impl ByteRingBuffer {
 
 impl Debug for ByteRingBuffer {
   fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-    f.debug_struct("ByteRingBuffer")
-        .field("dropped_count", &self.dropped_count)
-        .finish()?;
-    write!(f, ": ")?;
+    if self.dropped_count > 0 {
+      write!(f, "[missing {} bytes...] ", self.dropped_count)?;
+    }
     for b in &self.data {
       write!(f, "{b:02X} ")?;
     }

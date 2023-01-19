4425de0a06c1d8a3d88ecfaf18b3a710ec21e2ec
jasta <jasta@devtcg.org>
2023-01-18 19:47:57 -0800

    Work around uart_read_bytes having read_exact semantics
    I assumed this had read semantics like those in read() but turns out
    they're read_exact semantics by default so you need to do fancy stuff
    to read only from the RX buffer (or I guess read just once byte at a
    time but that seems really inefficient).

--- esp32-app/src/esp_uart_transport.rs
+++ esp32-app/src/esp_uart_transport.rs
@@ -1,8 +1,9 @@
+use std::cmp::{max, min};
 use std::io::{BufRead, ErrorKind};
 use std::ops::Deref;
 use std::sync::{Arc, Mutex};
 
-use esp_idf_hal::delay::BLOCK;
+use esp_idf_hal::delay::{BLOCK, NON_BLOCK};
 use esp_idf_hal::gpio::{AnyOutputPin, Gpio3, InputPin, Output, OutputPin, PinDriver, Pull};
 use esp_idf_hal::peripheral::{Peripheral};
 use esp_idf_hal::prelude::*;
@@ -74,7 +75,41 @@ impl Transport<EspUartRx, EspUartTx> for EspUartTransport {
 
 impl std::io::Read for EspUartRx {
   fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-    block!(rw_to_nb_std(self.rx_driver.read(buf, BLOCK)))
+    if buf.is_empty() {
+      return Ok(0)
+    }
+
+    // uart_read_bytes is implemented kinda funny such that if there is data available in the
+    // buffer it won't just return a short read but will instead always try to read up to the
+    // provided length (i.e. `buf.len()` in our case).  To combat this we'll read either
+    // the amount of data in the buffer _or_ just a single byte then drain the full buffer after.
+    let available = self.rx_driver.count().map_err(err_to_std)?;
+    if available == 0 {
+      let n = block!(rw_to_nb_std(self.rx_driver.read(&mut buf[0..1], BLOCK)))?;
+      assert_eq!(n, 1);
+
+      // Now let's try again with the RX buffer.
+      self.read_with_rx_buffer(&mut buf[1..]).map_err(err_to_std)
+    } else {
+      self.read_with_rx_buffer(buf).map_err(err_to_std)
+    }
+  }
+}
+
+impl EspUartRx {
+  /// Perform a UART read but _only_ take bytes from the RX buffer (i.e. do not wait
+  /// for more data to become available and return immediately if none are).
+  fn read_with_rx_buffer(&mut self, buf: &mut [u8]) -> Result<usize, EspError> {
+    let available = self.rx_driver.count()?;
+    if available == 0 {
+      Ok(0)
+    } else {
+      let max_len = min(available, buf.len());
+      match self.rx_driver.read(&mut buf[0..max_len], NON_BLOCK)? {
+        0 => panic!("Concurrent UART read detected!"),
+        n => Ok(n),
+      }
+    }
   }
 }
 
